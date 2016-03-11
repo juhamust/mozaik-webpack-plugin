6abd5e789111772eeb980172b096d9e028971e2d
Juha Mustonen <juha.mustonen@iki.fi>
2016-03-11 23:38:51 +0200

    Converted back to ES5 and worked with style reading

--- index.js
+++ index.js
@@ -1,80 +1,98 @@
-import _ from 'lodash';
-import fs from 'fs';
-import path from 'path';
-import listDirs from 'listdirs';
+var fs = require('fs');
+var path = require('path');
+var Promise = require('bluebird');
+var _  = require('lodash');
 
-class FileListPlugin {
+function FileListPlugin(options) {
+  options = options || {};
+  console.log('options', options);
+  this.plugins = [];
+  this.pluginDir = options.pluginDir;
 
-  constructor(options) {
-    options = options || {};
-    console.log('options', options);
-    this.plugins = [];
-    this.pluginDir = options.pluginDir;
+  if (!this.pluginDir) { return console.error('Provide pluginDir as option '); }
 
-    if (!this.pluginDir) { return console.error('Provide pluginDir as option '); }
+  this.plugins = _.chain(fs.readdirSync(this.pluginDir))
+    .filter(function(entry) {
+      return entry.match(/mozaik-ext-/);
+    })
+    .value();
 
-    this.plugins = _.chain(fs.readdirSync(this.pluginDir))
-      .filter((entry) => {
-        return entry.match(/mozaik-ext-/);
-      })
-      .value();
+  console.log('Plugins', this.plugins);
+  return;
 
-    console.log('Plugins', this.plugins);
-    return;
-
-    var ignored = ['node_modules'];
-    (this.pluginDir, (err, list) => {
-      if (err) { return console.error(err); }
-      console.log(list);
-    }, ignored);
-  }
-
-  _collectStyles(targetPath) {
-    _.each(this.plugins, (pluginName) => {
-      var styleFile = path.join(this.pluginDir, pluginName, 'styl/style.styl');
-      console.log('Style file', styleFile);
-    });
-  }
-
-  apply(compiler) {
-
-    compiler.plugin('emit', (compilation, callback) => {
-      // Create a header string for the generated file:
-      var filelist = 'In this build:\n\n';
-
-      this.collectStyles();
-
-      return callback();
-
-      console.log('context', compiler.context);
-
-      compilation.fileDependencies.push(path.join(compiler.context, 'foo.txt'));
-      compilation.fileDependencies.push('/Users/juha/Work/mozaik-webpack-plugin/test.js');
-
-      console.log('COMPILATION', compilation.fileDependencies, _.keys(compilation.assets));
-      return callback();
-
-
-
-      // Loop through all compiled assets,
-      // adding a new line item for each filename.
-      for (var filename in compilation.assets) {
-        filelist += ('- '+ filename +'\n');
-      }
-
-      // Insert this list into the Webpack build as a new file asset:
-      compilation.assets['filelist.md'] = {
-        source: () => {
-          return filelist;
-        },
-        size: () => {
-          return filelist.length;
-        }
-      };
-
-      callback();
-    });
-  }
+  var ignored = ['node_modules'];
+  (this.pluginDir, function(err, list) {
+    if (err) { return console.error(err); }
+    console.log(list);
+  }, ignored);
 }
 
-export default FileListPlugin;
+FileListPlugin.prototype._readFile = function(path) {
+  return new Promise(function(resolve, reject) {
+    fs.access(path, fs.F_OK, function(err) {
+      if (err) {
+        return resolve('');
+      }
+      return resolve(require(path));
+      //return resolve(fs.readFileSync(path).toString());
+    })
+  });
+
+}
+
+FileListPlugin.prototype._collectStyles = function(targetPath) {
+  var _this = this;
+
+  return Promise.all(_.map(this.plugins, function(pluginName) {
+    return _this._readFile(path.join(_this.pluginDir, pluginName, 'styl/index.styl'));
+  }))
+  .then(function(fileContents) {
+    console.log('OUT', fileContents);
+  });
+}
+
+FileListPlugin.prototype.apply = function(compiler) {
+  var _this = this;
+
+  compiler.plugin('emit', function(compilation, callback) {
+    // Create a header string for the generated file:
+    var filelist = 'In this build:\n\n';
+
+    _this._collectStyles();
+
+    return callback();
+
+
+    // ------
+
+    console.log('context', compiler.context);
+
+    compilation.fileDependencies.push(path.join(compiler.context, 'foo.txt'));
+    compilation.fileDependencies.push('/Users/juha/Work/mozaik-webpack-plugin/test.js');
+
+    console.log('COMPILATION', compilation.fileDependencies, _.keys(compilation.assets));
+    return callback();
+
+
+
+    // Loop through all compiled assets,
+    // adding a new line item for each filename.
+    for (var filename in compilation.assets) {
+      filelist += ('- '+ filename +'\n');
+    }
+
+    // Insert this list into the Webpack build as a new file asset:
+    compilation.assets['filelist.md'] = {
+      source: function() {
+        return filelist;
+      },
+      size: function() {
+        return filelist.length;
+      }
+    };
+
+    callback();
+  });
+}
+
+module.exports = FileListPlugin;
